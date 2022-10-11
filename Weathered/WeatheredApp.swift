967cdb1db668263bbd388f6e75b84c343bb686de
nastechi <ad.boriskina@icloud.com>
2022-10-11 14:24:38 +0500

    Initial Commit

---
+++ Weathered/WeatheredApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatheredApp.swift
+//  Weathered
+//
+//  Created by Анастасия on 11.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatheredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
